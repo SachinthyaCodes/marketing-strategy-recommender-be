75a316772aae90c5393d62e0a609f316cb749273
Sachinthya Lakshitha <127582741+SachinthyaCodes@users.noreply.github.com>
2026-03-07 10:25:51 +0530

    Use CPU-only torch to avoid 3GB CUDA download on Render

--- build.sh
+++ build.sh
@@ -2,6 +2,11 @@
 set -e
 
 pip install --upgrade pip
+
+# Install CPU-only torch FIRST so sentence-transformers doesn't pull the CUDA version (~3 GB)
+pip install torch --extra-index-url https://download.pytorch.org/whl/cpu
+
+# Install the rest of the dependencies
 pip install -r requirements.txt
 
 # Pre-download the sentence-transformers model so startup is fast
